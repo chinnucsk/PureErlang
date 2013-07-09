c5073531d984a54fe9ca5bd0c58f9fb337d77fcc
Rocky Bevins <rockybevins@gmail.com>
2013-07-09 15:26:15 -0400

    Removed testing controller cleaned up controller

--- src/controller/pureerlang_pages_controller.erl
+++ src/controller/pureerlang_pages_controller.erl
@@ -1,4 +1,4 @@
--module(PureErlang_pages_controller, [Req]).
+-module(pureerlang_pages_controller, [Req]).
 -compile(export_all).
  
 index('GET', []) ->
@@ -8,45 +8,45 @@ index('GET', []) ->
 %% @doc Handles rendering the new wiki page view which is empty by default
 create('GET', []) -> ok;
 
-%% @doc Handles POST data of form submission from new wiki page
+%% @doc Handles POST data for creating pages
 create('POST', []) ->
     Title = Req:post_param("page_title"),
     Text = Req:post_param("page_text"),
-    NewWikiPage = page:new(id, Title, Text),
-    case NewWikiPage:save() of
-        {ok, SavedWikiPage} ->   {redirect, [{action, "view"}, {id, SavedWikiPage:id()}]};
-        {error, ErrorList} -> {ok, [{errors, ErrorList}, {new_page, NewWikiPage}]}
+    NewPage = page:new(id, Title, Text),
+    case NewPage:save() of
+        {ok, SavedPage} ->   {redirect, [{action, "view"}, {id, SavedPage:id()}]};
+        {error, ErrorList} -> {ok, [{errors, ErrorList}, {new_page, NewPage}]}
     end.
 
-%% @doc display a specific wiki page
+%% @doc display a specific page
 view('GET', [Id]) ->
     case boss_db:find(Id) of
         {error, Reason} -> {redirect, [{action, "create"}]}; %% TODO: Redirect to error page
         undefined -> {redirect, [{action, "create"}]}; % When you visit /view/NotExistentPage the requested Page doesn't exist, we redirect the client to the edit Page so the content may be created
-        ExistingWikiPage ->
+        ExistingPage ->
             % Replace all [page-id] with links
             % TODO: There has to be a better way
-            StartHrefs = re:replace(ExistingWikiPage:page_text(), "\\[\\w*\-*[0-9]*", "<a href='/pages/view/&'>&", [global, {return, list}]),
+            StartHrefs = re:replace(ExistingPage:page_text(), "\\[\\w*\-*[0-9]*", "<a href='/pages/view/&'>&", [global, {return, list}]),
             ClosedHrefs = re:replace(StartHrefs, "\\]", "</a>", [global, {return, list}]),
             CleanedUp = re:replace(ClosedHrefs, <<"\\[">>, "", [global, {return, list}]),
-            {ok, [{page, ExistingWikiPage}, {cleaned, CleanedUp}]}
+            {ok, [{page, ExistingPage}, {cleaned, CleanedUp}]}
     end.
 
-%% @doc Fetch the existing wiki and show the edit page
+%% @doc edit a saved page
 edit('GET', [Id]) ->
-    ExistingWikiPage = boss_db:find(Id),
-    {ok, [{page, ExistingWikiPage}]};  
+    ExistingPage = boss_db:find(Id),
+    {ok, [{page, ExistingPage}]};  
 
 %% @doc Updates the wiki page from the Edit view's POST information
 edit('POST', []) ->
     Id = Req:post_param("page_id"),
     Title = Req:post_param("page_title"),
     Text = Req:post_param("page_text"),
-    ExistingWikiPage = boss_db:find(Id),
-    UpdatedWikiPage = ExistingWikiPage:set( [{page_text, Text}, {page_title, Title}] ),
-    case UpdatedWikiPage:save() of
-        {ok, SavedWiki} ->   {redirect, [{action, "view"}, {id, Id}]}; % Redirect to the updated page
-        {error, ErrorList} -> {ok, [{errors, ErrorList}, {page, UpdatedWikiPage}]}
+    ExistingPage = boss_db:find(Id),
+    UpdatedPage = ExistingPage:set( [{page_text, Text}, {page_title, Title}] ),
+    case UpdatedPage:save() of
+        {ok, Saved} ->   {redirect, [{action, "view"}, {id, Id}]}; % Redirect to the updated page
+        {error, ErrorList} -> {ok, [{errors, ErrorList}, {page, UpdatedPage}]}
     end.
 
 notfound('GET', []) -> ok;
--- src/controller/pureerlang_test_controller.erl
+++
@@ -1,5 +0,0 @@
--module(pureerlang_test_controller, [Req]).
--compile(export_all).
- 
-hello('GET', []) ->
-    {output, "<strong>Rocky says hello!</strong>"}.
